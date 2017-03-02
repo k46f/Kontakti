fb2358a89872b9ddf5270d5d11c3a4b676c406ea
John Ramirez <k46f@hotmail.com>
2017-03-02 17:18:50 -0500

    Declare full name textview and imageview photo in viewcontact

--- app/src/main/java/io/github/k46f/kontakti/ViewContact.java
+++ app/src/main/java/io/github/k46f/kontakti/ViewContact.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ViewContact extends AppCompatActivity {
 
-    private TextView phoneView, adressView, emailView, facebookView, birthdayView;
+    private TextView phoneView, adressView, emailView, facebookView, birthdayView, fullName;
+    private ImageView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
