b954dd1b42e7b417af3ee1ab44a1bdd2c7eeaf1b
John Ramirez <k46f@hotmail.com>
2017-03-02 17:29:56 -0500

    Declare EditText, ImageView and "Change Photo" button in edit contact.

--- app/src/main/java/io/github/k46f/kontakti/EditContact.java
+++ app/src/main/java/io/github/k46f/kontakti/EditContact.java
@@ -2,8 +2,15 @@ package io.github.k46f.kontakti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class EditContact extends AppCompatActivity {
+
+    private Button changePhotoButton;
+    private ImageView photoView;
+    private EditText nameText, phoneText, addressText, emailText, facebookText, birthdayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
