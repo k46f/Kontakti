1d750070b48ae60b80c18833ad719d27916799b0
John Ramirez <k46f@hotmail.com>
2017-03-02 17:42:26 -0500

    Initialize EditText, Button and ImageView in EditContact.java

--- app/src/main/java/io/github/k46f/kontakti/EditContact.java
+++ app/src/main/java/io/github/k46f/kontakti/EditContact.java
@@ -16,5 +16,14 @@ public class EditContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact);
-    }
+
+        changePhotoButton = (Button) findViewById(R.id.changePhotoButton);
+        photoView = (ImageView) findViewById(R.id.photoView);
+        nameText = (EditText) findViewById(R.id.nameText);
+        phoneText = (EditText) findViewById(R.id.phoneText);
+        addressText = (EditText) findViewById(R.id.addressText);
+        emailText = (EditText) findViewById(R.id.emailText);
+        facebookText = (EditText) findViewById(R.id.facebookText);
+        birthdayText = (EditText) findViewById(R.id.birthdayText);
+     }
 }
