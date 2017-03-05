f2109ef5098fd33565fdae9e1d444ea09196428b
John Ramirez <k46f@hotmail.com>
2017-03-05 13:34:43 -0500

    Add Method to write new contact to database

--- app/src/main/java/io/github/k46f/kontakti/NewContact.java
+++ app/src/main/java/io/github/k46f/kontakti/NewContact.java
@@ -1,16 +1,20 @@
 package io.github.k46f.kontakti;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NewContact extends AppCompatActivity {
 
-    private Button selectPhotoButton;
+    private Button selectPhotoButton, saveButton;
     private ImageView photoView;
     private EditText nameText, phoneText, addressText, emailText, facebookText, birthdayText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,35 @@ public class NewContact extends AppCompatActivity {
         emailText = (EditText) findViewById(R.id.emailText);
         facebookText = (EditText) findViewById(R.id.facebookText);
         birthdayText = (EditText) findViewById(R.id.birthdayText);
+        saveButton = (Button) findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+
+                String textName = nameText.getText().toString();
+                String textPhone = phoneText.getText().toString();
+                String textAddress = addressText.getText().toString();
+                String textEmail = emailText.getText().toString();
+                String textFacebook = facebookText.getText().toString();
+                String textBirthday = birthdayText.getText().toString();
+
+                try {
+                    DataBaseManager dbm = new DataBaseManager(context);
+                    dbm.openDb();
+                    long result = dbm.register(textName, textPhone, textAddress, textEmail, textFacebook,
+                            textBirthday);
+                    dbm.closeDb();
+                    if (result > 0) {
+                        Toast kToast = Toast.makeText(context, "Success!!!", Toast.LENGTH_LONG);
+                        kToast.show();
+                    }
+                } catch (Exception e) {
+                    Toast kToast = Toast.makeText(context, e.toString(), Toast.LENGTH_LONG);
+                    kToast.show();
+                }
+            }
+        });
     }
 }
