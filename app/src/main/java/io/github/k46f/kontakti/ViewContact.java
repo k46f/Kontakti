f948c4df44b53feadff7546732cbc421c8a859fb
John Ramirez <k46f@hotmail.com>
2017-03-26 21:34:59 -0500

    Add edit icon to app tool bar, and remove floating button

--- app/src/main/java/io/github/k46f/kontakti/ViewContact.java
+++ app/src/main/java/io/github/k46f/kontakti/ViewContact.java
@@ -6,6 +6,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -78,7 +80,7 @@ public class ViewContact extends AppCompatActivity {
         dbm.closeDb();
     }
 
-    public void editContact(View v) {
+    public void editContact(MenuItem mi) {
         Intent firstIntent = getIntent();
         String contactId = firstIntent.getStringExtra(MainActivity.CONTACT_ID);
 
@@ -97,6 +99,13 @@ public class ViewContact extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp(){
         finish();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_contact, menu);
         return true;
     }
 }
