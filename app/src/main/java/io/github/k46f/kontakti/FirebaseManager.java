3f8601da7c19358d52ec4e456396bbf265a3455b
John Ramirez <k46f@hotmail.com>
2017-04-05 19:37:43 -0500

    Add constructor tu FrebaseManager

--- app/src/main/java/io/github/k46f/kontakti/FirebaseManager.java
+++ app/src/main/java/io/github/k46f/kontakti/FirebaseManager.java
@@ -1,13 +1,15 @@
 package io.github.k46f.kontakti;
 
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FirebaseManager {
 
-    String googleId;
+    private String googleId;
+    private FirebaseDatabase fDatabase;
 
     public FirebaseManager(String gId){
         googleId = gId;
-
-
+        fDatabase = FirebaseDatabase.getInstance();
     }
 }
