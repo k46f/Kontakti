0cb7c0087168026774f5760eed318d1315468143
John Ramirez <k46f@hotmail.com>
2017-04-05 20:13:02 -0500

    Add "users reference" to FirebaseManager

--- app/src/main/java/io/github/k46f/kontakti/FirebaseManager.java
+++ app/src/main/java/io/github/k46f/kontakti/FirebaseManager.java
@@ -1,15 +1,20 @@
 package io.github.k46f.kontakti;
 
 
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseManager {
 
+    private final static String USERS_REFERENCE = "users";
+
     private String googleId;
     private FirebaseDatabase fDatabase;
+    private DatabaseReference usersReference;
 
     public FirebaseManager(String gId){
         googleId = gId;
         fDatabase = FirebaseDatabase.getInstance();
+        usersReference = fDatabase.getReference(USERS_REFERENCE);
     }
 }
